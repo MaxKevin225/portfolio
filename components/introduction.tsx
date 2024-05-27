2e67db633fc6f9b6ebe797d39cdc695553c9b9b6
Max.Group <limk69966@gmail.com>
2024-05-27 06:30:40 -0700

    Update introduction.tsx

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -8,7 +8,7 @@ const Introduction = () => {
             <div className="z-20 grid items-center h-full p-6 py-20 md:py-0 md:grid-cols-2">
                 <Image src="/home-4.png" priority width="800" height="800" alt="Avatar" style={{opacity:"0.7"}} />
                 <div className="flex flex-col justify-center max-w-md">
-                    <h1 className="mb-5 text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-10">Hi There, I'M KEVIN LIM <br />
+                    <h1 className="mb-5 text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-10">Hi There, I AM KEVIN LIM <br />
                         <TypeAnimation
                             sequence={[
                                 "software developer",
